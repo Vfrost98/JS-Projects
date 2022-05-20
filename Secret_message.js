ec42485ac70c77471e4a7a5396e2409012b12916
VeeisMe <41344497+Vfrost98@users.noreply.github.com>
2022-05-20 00:00:35 -0500

    Create Secret_message.js

---
+++ Secret_message.js
@@ -0,0 +1,18 @@
+/*
+  Task:
+    use an array of methods ot transform an array of strings into a secret message
+*/
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'easily';
+secretMessage.shift();
+secretMessage.unshift('Programming'); //adds word to the front of an array
+
+secretMessage.splice(6, 10, 'know');
+
+console.log(secretMessage.join(' ')); //join made it a sentence
